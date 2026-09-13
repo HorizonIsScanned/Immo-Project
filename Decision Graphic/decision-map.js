fcd760ca7ec7685dfc6761533ed66b90a941ed73
HorizonIsScanned <christopher.bierbrauer@gmail.com>
2026-09-13 17:28:50 +0200

    picture bug

--- Decision Graphic/decision-map.js
+++ Decision Graphic/decision-map.js
@@ -274,7 +274,7 @@
 </section>`;
   var STYLE_ID = 'ipd-decision-map-styles';
   var MOUNT_ID = 'ipd-decision-map';
-  var PHOTO_FILE = 'assets/Joerg_Headshot1.png';
+  var PHOTO_FILE = '../assets/Joerg_Headshot1.png';
   var FALLBACK_PHOTO = 'https://cdn.prod.website-files.com/6a44ebdb190d2f1ca6ddf4a7/6a53db2e9363359153fd42de_jvbl-p-1080.jpg';
   var script = document.currentScript;
 
